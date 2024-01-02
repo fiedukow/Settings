030b6c7efb1ca25a1eafda0367be961d6537fb73
Andrzej Fiedukowicz <fiedukow@gmail.com>
2024-01-02 14:17:45 +0100

    Not final

--- Sources/Settings/Pane.swift
+++ Sources/Settings/Pane.swift
@@ -47,7 +47,7 @@ extension Settings {
 	/**
 	Hosting controller enabling `Settings.Pane` to be used alongside AppKit `NSViewController`'s.
 	*/
-	public final class PaneHostingController<Content: View>: NSHostingController<Content>, SettingsPane {
+	public class PaneHostingController<Content: View>: NSHostingController<Content>, SettingsPane {
 		public let paneIdentifier: PaneIdentifier
 		public let paneTitle: String
 		public let toolbarItemIcon: NSImage
